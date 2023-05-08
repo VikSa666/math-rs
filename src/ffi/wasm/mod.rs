ef357b6c46901009ef6bf558023649b50dc9f5f4
victor-at-wiris <victor.santiago@wiris.com>
2023-05-08 13:02:10 +0200

    chore: adapt wasm exposed functionalities

--- src/ffi/wasm/mod.rs
+++ src/ffi/wasm/mod.rs
@@ -1,7 +1,14 @@
 mod result;
 
-use crate::matrix::GenericMatrix;
+use crate::matrix::{GenericMatrix, Matrix};
 use wasm_bindgen::prelude::*;
+
+/// Initialization function that automatically gets called when the module is loaded in WASM.
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    tracing_wasm::set_as_global_default();
+    Ok(())
+}
 
 #[wasm_bindgen]
 pub struct MatrixUsize {
@@ -12,16 +19,56 @@ pub struct MatrixUsize {
 impl MatrixUsize {
     #[wasm_bindgen(constructor)]
     pub fn new(content: Vec<usize>, rows: usize, columns: usize) -> Result<MatrixUsize, JsValue> {
-        let inner = GenericMatrix::new(content, rows, columns);
+        if content.len() != rows * columns {
+            return Err(JsValue::from_str(
+                format!(
+                    "Cannot build matrix of dimensions {rows}x{columns} with {} elements!",
+                    content.len()
+                )
+                .as_str(),
+            ));
+        }
+        let mut matrix: Vec<Vec<usize>> = Vec::with_capacity(rows);
+        for i in 0..rows {
+            let mut row: Vec<usize> = Vec::with_capacity(columns);
+            for j in 0..columns {
+                row.push(content[i * (columns - 1) + j])
+            }
+            matrix.push(row)
+        }
+        let inner = GenericMatrix::new(matrix);
         match inner {
-            Ok(inner) => Ok(MatrixUsize { inner }),
+            Ok(inner) => {
+                tracing::info!("Matrix has been built correctly");
+                Ok(MatrixUsize { inner })
+            }
             Err(error) => Err(JsValue::from(error)),
         }
     }
+    //     let inner = GenericMatrix::new(content);
+    //     match inner {
+    //         Ok(inner) => Ok(MatrixUsize { inner }),
+    //         Err(error) => Err(JsValue::from(error)),
+    //     }
+    // }
+
+    #[wasm_bindgen(getter)]
+    pub fn rows(&self) -> usize {
+        self.inner.rows()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn columns(&self) -> usize {
+        self.inner.columns()
+    }
+
+    pub fn get(&self, row: usize, column: usize) -> Result<usize, JsValue> {
+        let result = self.inner.get(row + 1, column + 1)?;
+        Ok(result.clone())
+    }
 
     pub fn sum(matrix_a: MatrixUsize, matrix_b: MatrixUsize) -> Result<MatrixUsize, JsValue> {
-        Ok(MatrixUsize {
-            inner: (matrix_a.inner + matrix_b.inner)?,
-        })
+        let sum = (matrix_a.inner + matrix_b.inner)?;
+        Ok(MatrixUsize { inner: sum })
     }
 }
