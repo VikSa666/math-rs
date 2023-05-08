44cb739dbc42147d475afe918ce761ceb43834dd
victor-at-wiris <victor.santiago@wiris.com>
2023-05-08 13:01:43 +0200

    chore: use matrix api in dev server

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,15 +1,15 @@
-import _ from 'lodash';
-import printMe from './print';
+import _ from "lodash";
+import { printMe, sumMatrix } from "./print";
 
 function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = sumMatrix;
 
   element.appendChild(btn);
 
