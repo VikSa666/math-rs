8b1ea119ad224f452e5987127c8b1ea99fdfb94f
victor-at-wiris <victor.santiago@wiris.com>
2023-05-11 17:24:40 +0200

    feat(dev): implement Vue dev server to display dev server

---
+++ dev/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <h1>Marhaba!</h1>
+  <div>
+    <textarea name="matA" id="matA" cols="30" rows="10" v-model="matA" />
+    <textarea name="matB" id="matB" cols="30" rows="10" v-model="matB" />
+    <span>=></span>
+    <textarea
+      name="result"
+      id="result"
+      cols="30"
+      rows="10"
+      v-model="result"
+    ></textarea>
+  </div>
+  <div>
+    <button @click="performSum">+</button>
+    <button @click="performSub">-</button>
+    <button @click="performMul">*</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import init, { MatrixUsize } from "math-rs";
+import { ref } from "vue";
+
+const matA = ref("");
+const matB = ref("");
+const result = ref("Nothing yet...");
+
+const performSum = async () => {
+  await init();
+  result.value = MatrixUsize.sum(
+    MatrixUsize.from_string(matA.value),
+    MatrixUsize.from_string(matB.value)
+  ).to_string();
+};
+
+const performSub = async () => {
+  await init();
+  alert("Not yet implemented");
+};
+
+const performMul = async () => {
+  await init();
+  alert("Not yet implemented");
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- dev/src/index.js
+++ dev/src/index.js
@@ -1,15 +1,21 @@
 import _ from "lodash";
 import { printMe, sumMatrix } from "./print";
 
-function component() {
+async function component() {
   const element = document.createElement("div");
-  const btn = document.createElement("button");
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   btn.innerHTML = "Click me and check the console!";
-  btn.onclick = sumMatrix;
+  const matA = document.getElementById("matA").textContent;
+  const matB = document.getElementById("matB").textContent;
+  const sum = (document.getElementById("sumButton").onclick = await sumMatrix(
+    matA,
+    matB
+  ));
+
+  document.getElementById("result").textContent = sum;
 
   element.appendChild(btn);
 
