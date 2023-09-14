759ad80397690be5352ebae11999216e84c0d1ca
VikSa666 <92550917+VikSa666@users.noreply.github.com>
2023-09-14 11:29:11 +0000

    chore: change parameters to be reference of s instead of having property

--- src/arithmetics/euclid.rs
+++ src/arithmetics/euclid.rs
@@ -1,28 +1,29 @@
 use crate::{
+    equality::Equals,
     identities::Zero,
-    structures::{integers::Integer, Ring}, equality::Equals,
+    structures::{integers::Integer, Ring},
 };
 
-pub fn gcd<R>(a: Integer<R>, b: Integer<R>) -> Integer<R>
+pub fn gcd<R>(a: &Integer<R>, b: &Integer<R>) -> Integer<R>
 where
     R: Ring,
 {
     if b.equals(&Integer::zero(), 0.) {
-        return a;
+        return a.clone();
     }
-    gcd(b, a % b)
+    gcd(b, &(a.clone() % b.clone()))
 }
 
-pub fn euclidean_division<R>(a: Integer<R>, b: Integer<R>) -> (Integer<R>, Integer<R>)
+pub fn euclidean_division<R>(a: &Integer<R>, b: &Integer<R>) -> (Integer<R>, Integer<R>)
 where
     R: Ring,
 {
-    let q = a / b;
-    let r = a - q.clone() * b;
+    let q = a.to_owned() / b.to_owned();
+    let r = a.to_owned() - q.clone() * b.to_owned();
     (q, r)
 }
 
-pub fn quotient<R>(a: Integer<R>, b: Integer<R>) -> Integer<R>
+pub fn quotient<R>(a: &Integer<R>, b: &Integer<R>) -> Integer<R>
 where
     R: Ring,
 {
@@ -37,13 +38,13 @@ mod test {
     fn test_euclid() {
         let a = Integer::<isize>::new(1);
         let b = Integer::<isize>::new(1);
-        assert_eq!(super::gcd(a, b), Integer::<isize>::new(1));
+        assert_eq!(super::gcd(&a, &b), Integer::<isize>::new(1));
     }
 
     #[test]
     fn test_gcd() {
         let a = Integer::<isize>::new(252);
         let b = Integer::<isize>::new(105);
-        assert_eq!(super::gcd(a, b), Integer::<isize>::new(21));
+        assert_eq!(super::gcd(&a, &b), Integer::<isize>::new(21));
     }
 }
