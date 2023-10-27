f2a8cf101d03573ed6b0b57b86215095bbe08a2b
VikSa666 <92550917+VikSa666@users.noreply.github.com>
2023-10-27 08:42:45 +0000

    chore: uncomment bench

--- benches/matrix.rs
+++ benches/matrix.rs
@@ -1,34 +1,34 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use math_rs::matrix::generic::Matrix;
 
-// fn bench_sum(c: &mut Criterion) {
-//     let mut group = c.benchmark_group("Sum 3x3 matrix");
-//     for i in [20u64, 21u64].iter() {
-//         let mat_a = vec![vec![1; *i as usize]; *i as usize];
-//         let mat_b = vec![vec![-1; *i as usize]; *i as usize];
-//         group.bench_with_input(BenchmarkId::new("With structure", i), i, |b, _| {
-//             b.iter(|| {
-//                 let matrix_a = Matrix {
-//                     data: mat_a.clone(),
-//                 };
-//                 let matrix_b = Matrix {
-//                     data: mat_b.clone(),
-//                 };
-//                 let _ = matrix_a + matrix_b;
-//             })
-//         });
-//         group.bench_with_input(BenchmarkId::new("Native", i), i, |b, _| {
-//             b.iter(|| {
-//                 for i in 0..3 {
-//                     for j in 0..3 {
-//                         let _ = mat_a[i][j] + mat_b[i][j];
-//                     }
-//                 }
-//             })
-//         });
-//     }
-//     group.finish();
-// }
+fn bench_sum(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Sum 3x3 matrix");
+    for i in [20u64, 21u64].iter() {
+        let mat_a = vec![vec![1; *i as usize]; *i as usize];
+        let mat_b = vec![vec![-1; *i as usize]; *i as usize];
+        group.bench_with_input(BenchmarkId::new("With structure", i), i, |b, _| {
+            b.iter(|| {
+                let matrix_a = Matrix {
+                    data: mat_a.clone(),
+                };
+                let matrix_b = Matrix {
+                    data: mat_b.clone(),
+                };
+                let _ = matrix_a + matrix_b;
+            })
+        });
+        group.bench_with_input(BenchmarkId::new("Native", i), i, |b, _| {
+            b.iter(|| {
+                for i in 0..3 {
+                    for j in 0..3 {
+                        let _ = mat_a[i][j] + mat_b[i][j];
+                    }
+                }
+            })
+        });
+    }
+    group.finish();
+}
 
 fn bench_sum_with_incrementing_dimensions(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sum matrix of growing size");
@@ -56,5 +56,5 @@ fn bench_sum_with_incrementing_dimensions(c: &mut Criterion) {
     group.finish()
 }
 
-criterion_group!(benches, bench_sum_with_incrementing_dimensions);
+criterion_group!(benches, bench_sum_with_incrementing_dimensions, bench_sum);
 criterion_main!(benches);
